703be189724ca5096a16cd2c8537077c7f1b3cae
Miny1110 <wjdalswjd453@naver.com>
2022-08-29 16:24:50 +0900

    0829 오후

---
+++ src/main/java/com/exe/aop/AopMain.java
@@ -0,0 +1,28 @@
+package com.exe.aop;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class AopMain {
+
+	public static void main(String[] args) {
+
+		GenericXmlApplicationContext context =
+				new GenericXmlApplicationContext("app-context.xml");
+		
+		TargetA ta = (TargetA)context.getBean("targetA");
+		
+		ta.doSomething1();
+		ta.doAnother2();
+		ta.doAnother1();
+		ta.doAnother2();
+		
+		TargetB tb = (TargetB)context.getBean("targetB");
+		
+		tb.doSomething1();
+		tb.doAnother2();
+		tb.doAnother1();
+		tb.doAnother2();
+		
+	}
+
+}
